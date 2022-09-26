ba47fe6b7ad070a5c5f0f037e74d4d438f3d747d
raviteja4812023 <111496484+raviteja4812023@users.noreply.github.com>
2022-09-26 23:21:49 +0530

    Update Program-1.py

--- Program-1.py
+++ Program-1.py
@@ -1,1 +1,25 @@
+class calculator:
+    def addition(self):
+        print(a+b)
+    def subtraction(self):
+        print(a-b)
+    def multiplication(self):
+        print(a*b)
+    def divison(self):
+        print(a/b)
 
+a=float(input())
+b=float(input())
+
+cal=calculator()
+
+operation=input()
+
+if(operation == 'Addition'):
+    cal.addition()
+elif(operation == 'Subtraction'):
+    cal.subraction()
+elif(operation == 'Multiplication'):
+    cal.multiplication()
+elif(operation == 'Division'):
+    cal.divison()
