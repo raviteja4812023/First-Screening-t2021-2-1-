7476ff41b83c9aa59269bbad50615be6a449ffb8
raviteja4812023 <111496484+raviteja4812023@users.noreply.github.com>
2022-09-29 13:21:01 +0530

    Update Program-3.py

--- Program-3.py
+++ Program-3.py
@@ -1,1 +1,14 @@
-
+def integer(a):
+    i=1
+    j=0
+    items=[]
+    if(a%2==0):
+        a=a-1
+    while(j<a):
+        if(i%2!=0):
+            j=j+1
+            items.append(i)
+        i=i+1
+    print(items)
+a=int(input())
+integer(a)
